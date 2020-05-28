adf5ad5305a975a093afbbf395e8cb1d858e78fc
Nicholas George <nicholas.george32@gmail.com>
2020-05-27 21:02:15 -0600

    add a plot class and dummy button for plotting

--- abf_explorer/gui.py
+++ abf_explorer/gui.py
@@ -2,6 +2,10 @@
 # https://doc.qt.io/qtforpython/PySide2/QtWidgets/QWidget.html
 # https://doc.qt.io/qt-5/layout.html
 # https://pythonbasics.org/pyqt-grid/
+# examples python -m pyqtgraph.examples
+# CONVENTIONS!
+# if it is a widget, it has widget in the name.
+
 import sys
 import random
 import numpy as np
@@ -19,27 +23,42 @@ class MyWidget(qt.QWidget):
         #self.layout = self.s.QVBoxLayout()
         self.show()
 
-class Main:
-    def __init__(self, cmdflags):
-        self.mainApp = qt.QApplication([]) # command line flags if parsing
-        self.baseWidget = qt.QWidget()
-        self.leftSide = qt.QLabel()
-        self.leftSide.setText("Left side")
+
+class PlotWidget(pg.GraphicsWindow):
+    def __init__(self, parent):
+        super().__init__(parent = parent)
         self.x = np.linspace(0,100, 20)
         self.y = np.random.randn(20)
-        self.plotSide = pg.GraphicsWindow(title="test plot window")
-        self.plot = self.plotSide.addPlot(title="plot", x=self.x, y=self.y)
-        self.baseWidget.setWindowTitle("abf explorer")
-        self.baseLayout = qt.QGridLayout()
-        self.baseLayout.addWidget(self.leftSide, 0,0)
-        self.baseLayout.addWidget(self.plotSide, 0,1)
-        # another widget
+        self.mainPlot = self.addPlot(title="main plot test")
 
-        self.baseWidget.setLayout(self.baseLayout)
-        self.baseWidget.setGeometry(50,50,600,400)
-        self.baseWidget.show()
+    def plot_points(plotdict):
+        self.mainPlot.plot(plotdict['x'], plotdict['y'])
+
+class ABFExplorer:
+    def __init__(self, cmdflags):
+        self.mainApp = qt.QApplication([]) # command line flags if parsing
+        self.mainWindow= qt.QMainWindow()
+        self.centralWidget = qt.QWidget()
+        self.mainWindow.setCentralWidget(self.centralWidget)
+
+        self.leftSide = qt.QPushButton("Plot")
+
+        self.bottomSide = qt.QLabel()
+        self.bottomSide.setText("Bottom side")
+
+        self.plotWidget = PlotWidget(parent=self.centralWidget)
+        self.mainWindow.setWindowTitle("abf explorer")
+        self.mainLayout = qt.QGridLayout()
+        # main widget layout
+        self.mainLayout.addWidget(self.leftSide, 0,0)
+        self.mainLayout.addWidget(self.plotWidget, 0,1)
+        self.mainLayout.addWidget(self.bottomSide, 1,0)
+        # another widget
+        self.centralWidget.setLayout(self.mainLayout)
+        self.mainWindow.setGeometry(50,50,600,400)
+        self.mainWindow.show()
         self.mainApp.exec_()
 
 if __name__ == "__main__":
-        Main(sys.argv)
+        ABFExplorer(sys.argv)
         print("Closing")
