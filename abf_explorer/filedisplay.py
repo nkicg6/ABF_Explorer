913f5e266677ccf771848bbd5ba57f22cc6dcd29
Nicholas George <nicholas.george32@gmail.com>
2020-05-29 11:33:14 -0600

    black formatting

--- abf_explorer/filedisplay.py
+++ abf_explorer/filedisplay.py
@@ -5,13 +5,14 @@ import PyQt5.QtWidgets as qt
 
 DEBUG_DIR = "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-10-26"
 
+
 class FileDisplay(qt.QWidget):
     """controls display and file handling for file selection"""
 
     def __init__(self, parent):
         super().__init__(parent=parent)
         # VARS
-        self._var_workingDir = DEBUG_DIR # os.path.expanduser("~") # start home, replace with prev dir after selection
+        self._var_workingDir = DEBUG_DIR  # os.path.expanduser("~") # start home, replace with prev dir after selection
         self.var_selected_abf_files_dict = dict
 
         # button and display
@@ -28,25 +29,32 @@ class FileDisplay(qt.QWidget):
         self.button_select_abf.clicked.connect(self._choose_directory)
 
     def _choose_directory(self):
-        abf_dir = str(qt.QFileDialog.getExistingDirectory(self, "Select dir",
-                                                          self._var_workingDir))
+        abf_dir = str(
+            qt.QFileDialog.getExistingDirectory(
+                self, "Select dir", self._var_workingDir
+            )
+        )
         if not abf_dir:
             return
         self._filter_dir(abf_dir)
 
     def _filter_dir(self, abf_dir):
         self._var_workingDir = abf_dir
-        self.var_selected_abf_files_dict = {f:os.path.join(abf_dir, f) for f in os.listdir(abf_dir) if f.endswith(".abf")}
+        self.var_selected_abf_files_dict = {
+            f: os.path.join(abf_dir, f)
+            for f in os.listdir(abf_dir)
+            if f.endswith(".abf")
+        }
         self._populate_listbox_file_list()
 
     def _populate_listbox_file_list(self):
         self.listbox_file_list.clear()
         sorted_keys = sorted(self.var_selected_abf_files_dict.keys())
-        for n,f in enumerate(sorted_keys):
-            self.listbox_file_list.insertItem(n,f)
+        for n, f in enumerate(sorted_keys):
+            self.listbox_file_list.insertItem(n, f)
         default_selection = self.listbox_file_list.item(0)
         self.listbox_file_list.setCurrentItem(default_selection)
 
     def signal_item_changed():
-        #https://doc.qt.io/qt-5/qlistwidget.html#itemActivated
+        # https://doc.qt.io/qt-5/qlistwidget.html#itemActivated
         pass
--- abf_explorer/gui.py
+++ abf_explorer/gui.py
@@ -16,21 +16,21 @@ import pyqtgraph as pg
 
 from filedisplay import FileDisplay
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
+
 
 class PlotWidget(pg.GraphicsWindow):
     def __init__(self, parent):
-        super().__init__(parent = parent)
-        self.x = np.linspace(0,100, 20)
+        super().__init__(parent=parent)
+        self.x = np.linspace(0, 100, 20)
         self.y = np.random.randn(20)
         self.mainPlot = self.addPlot(title="main plot test")
 
     def update_plot(self, plotdict):
-        self.mainPlot.plot(plotdict['x'], plotdict['y'],
-                           name=plotdict['name'])
-        self.mainPlot.setTitle(plotdict['name'])
-        #self.mainPlot.addLegend()
+        self.mainPlot.plot(plotdict["x"], plotdict["y"], name=plotdict["name"])
+        self.mainPlot.setTitle(plotdict["name"])
+        # self.mainPlot.addLegend()
         print("Plotting called")
 
     def clear_plot(self, e=None):
@@ -50,49 +50,59 @@ class PlotControls(qt.QWidget):
         self.layout.addWidget(self.button_clear_plot, 1, 0)
         self.setLayout(self.layout)
 
+
 class ABFExplorer:
     """main abf explorer class contains all widgets and coordinates all actions"""
+
     def __init__(self, cmdflags):
-        self.mainApp = qt.QApplication([]) # command line flags if parsing
-        self.mainWindow= qt.QMainWindow()
+        self.mainApp = qt.QApplication([])  # command line flags if parsing
+        self.mainWindow = qt.QMainWindow()
         self.centralWidget = qt.QWidget()
         self.mainWindow.setCentralWidget(self.centralWidget)
         self.mainWindow.setWindowTitle("ABF explorer v0.0-dev")
 
         # make widgets
-        self.plotControlWidget= PlotControls(parent=self.centralWidget)
+        self.plotControlWidget = PlotControls(parent=self.centralWidget)
         self.plotWidget = PlotWidget(parent=self.centralWidget)
         self.fileExplorerWidget = FileDisplay(parent=self.centralWidget)
 
         # main widget layout
         self.mainLayout = qt.QGridLayout()
-        self.mainLayout.addWidget(self.fileExplorerWidget, 0,0)
-        self.mainLayout.addWidget(self.plotWidget, 0,1)
-        self.mainLayout.addWidget(self.plotControlWidget, 1,0)
+        self.mainLayout.addWidget(self.fileExplorerWidget, 0, 0)
+        self.mainLayout.addWidget(self.plotWidget, 0, 1)
+        self.mainLayout.addWidget(self.plotControlWidget, 1, 0)
         self.centralWidget.setLayout(self.mainLayout)
 
         # events
-        self.plotControlWidget.button_clear_plot.clicked.connect(self.plotWidget.clear_plot)
+        self.plotControlWidget.button_clear_plot.clicked.connect(
+            self.plotWidget.clear_plot
+        )
         self.plotControlWidget.button_plot.clicked.connect(self.TEMP_gen_data)
 
         # keyboard shortcuts
-        self.shortcut_update_plot = qt.QShortcut(QtGui.QKeySequence("Tab"),
-                                                 self.plotControlWidget.button_plot)
-        self.shortcut_clear_plot = qt.QShortcut(QtGui.QKeySequence("c"),
-                                                self.plotControlWidget.button_clear_plot)
+        self.shortcut_update_plot = qt.QShortcut(
+            QtGui.QKeySequence("Tab"), self.plotControlWidget.button_plot
+        )
+        self.shortcut_clear_plot = qt.QShortcut(
+            QtGui.QKeySequence("c"), self.plotControlWidget.button_clear_plot
+        )
         self.shortcut_update_plot.activated.connect(self.TEMP_gen_data)
         self.shortcut_clear_plot.activated.connect(self.plotWidget.clear_plot)
 
         # geometry adn run
-        self.mainWindow.setGeometry(50,50,600,400)
+        self.mainWindow.setGeometry(50, 50, 600, 400)
         self.mainWindow.show()
         self.mainApp.exec_()
 
     def TEMP_gen_data(self):
-        d = {'x':np.random.randn(20), 'y':np.random.randn(20),
-             'name':f'plot item {np.random.randn()}'}
+        d = {
+            "x": np.random.randn(20),
+            "y": np.random.randn(20),
+            "name": f"plot item {np.random.randn()}",
+        }
         self.plotWidget.update_plot(d)
 
+
 if __name__ == "__main__":
-        ABFExplorer(sys.argv)
-        print("Closing")
+    ABFExplorer(sys.argv)
+    print("Closing")
