40b02eb81af868fd99dce624248c05adcac9732e
Nicholas George <nicholas.george32@gmail.com>
2020-05-29 22:23:58 -0600

    add todo reminder

--- abf_explorer/gui.py
+++ abf_explorer/gui.py
@@ -5,7 +5,7 @@
 # examples python -m pyqtgraph.examples
 # plot item class https://pyqtgraph.readthedocs.io/en/latest/graphicsItems/plotitem.html
 # plot customizations for interaction https://pyqtgraph.readthedocs.io/en/latest/graphicsItems/plotitem.html
-# TODO! see choose file.
+# TODO! setup file info widget, have text input change when selection changes.
 
 import sys
 import random
