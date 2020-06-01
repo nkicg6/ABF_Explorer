8fe080a651ab87dc35ab8a747ef0a0e3b0293150
Nicholas George <nicholas.george32@gmail.com>
2020-06-01 08:25:39 -0600

    todos, new meta fields

--- abf_explorer/plotutils.py
+++ abf_explorer/plotutils.py
@@ -1,14 +1,19 @@
 # utilities for interacting with pyabf
+# would like a simpler way to handle IO. So when you select the ABF, we should store the ABF somewhere? or store the path to the ABF somewhere along with something like n_sweeps, n_channels, +metadata? this should be included in the metadata, along with the full path.
 import os
 import pyabf
 
+METADATA = {
+        "short_filename": str,
+        "full_path":str,
+        "sampling_frequency_khz": str,
+        "protocol": str,
+        "n_sweeps": int,
+        "n_channels":int,}
 
 def io_get_metadata(abf_path):
-    metadata = {
-        "short_filename": "",
-        "sampling_frequency_khz": "",
-        "protocol": "",
-    }
+    metadata = METADATA.copy()
+
     if not abf_path.endswith(".abf"):
         return metadata_error("string is not an abf: {abf_path}")
     if not os.path.exists(abf_path):
@@ -18,11 +23,12 @@ def io_get_metadata(abf_path):
     try:
         abf = io_read_abf(abf_path, loaddata=False)
         metadata["short_filename"] = os.path.split(abf_path)[-1]
+        metadata["full_path"] = abf_path
         metadata["sampling_frequency_khz"] = str(abf.dataRate / 1000)
         metadata["protocol"] = str(abf.protocol)
         return metadata
     except AssertionError as e:
-        return metadata_error(e)
+        return metadata_error(e, abf_path)
 
 
 def io_read_abf(abf_path, loaddata):
@@ -35,13 +41,13 @@ def io_read_abf(abf_path, loaddata):
         raise AssertionError(error_str)
 
 
-def metadata_error(error):
+def metadata_error(error, attempted_path):
     print("[metadata_error] returning blank metadata")
-
-    metadata = {
-        "short_filename": "",
-        "sampling_frequency_khz": "",
-        "protocol": "",
-        "error": error,
-    }
+    metadata = METADATA.copy()
+    metadata['error'] = error
+    metadata['full_path'] = attempted_path
     return metadata
+
+
+def io_get_sweep(opts_dict):
+    abf = io_read_abf()
