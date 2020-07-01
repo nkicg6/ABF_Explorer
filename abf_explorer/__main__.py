5f895bc206e00798f741d6d2547a005b9c2f2da4
Nicholas George <nicholas.george32@gmail.com>
2020-06-30 21:30:13 -0600

    add a context so imports from top level though started from within.

--- abf_explorer/__main__.py
+++ abf_explorer/__main__.py
@@ -1,5 +1,9 @@
-# import logging
+import os
 import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)  # hack for tests for now..
 import PyQt5.QtWidgets as qt
 from args import parser
 from gui import ABFExplorer
