3226ab95bc883d3503cc2bab8a9a6fff1d210be3
Nicholas George <nicholas.george32@gmail.com>
2020-06-30 22:52:07 -0600

    add startup tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,5 +1,22 @@
 from . import context
 import pytest
+import PyQt5.QtWidgets as qt
 from abf_explorer import gui
+from abf_explorer.args import parser
 
-print("done")
+app = qt.QApplication([])
+
+
+def test_startup_no_startup_dir():
+    cmd_args = parser.parse_args()
+    explorer = gui.ABFExplorer(startup_dir=cmd_args.startup_dir)
+    assert explorer.fileExplorerWidget.listbox_file_list.count() == 0
+
+
+def test_startup_non_existant_dir():
+    cmd_args = parser.parse_args(["-d" "I_dont_exist"])
+    explorer = gui.ABFExplorer(startup_dir=cmd_args.startup_dir)
+    assert (
+        explorer.fileExplorerWidget.listbox_file_list.item(0).text()
+        == "Nothing here..."
+    )
