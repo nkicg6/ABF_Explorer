be99dab95c5dfeab6adeef278eb491a6653c76c9
Nicholas George <nicholas.george32@gmail.com>
2020-07-01 09:09:46 -0600

    added real data test for startup

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,3 +1,4 @@
+import os
 from . import context
 import pytest
 import PyQt5.QtWidgets as qt
@@ -5,6 +6,8 @@ from abf_explorer import gui
 from abf_explorer.args import parser
 
 app = qt.QApplication([])
+
+ABF_DATA_DIR = "data/abfs"
 
 
 def test_startup_no_startup_dir():
@@ -19,4 +22,22 @@ def test_startup_non_existent_dir():
     assert (
         explorer.fileExplorerWidget.listbox_file_list.item(0).text()
         == "Nothing here..."
+    )
+
+
+def test_startup_real_dir():
+    cmd_args = parser.parse_args(["-d", ABF_DATA_DIR])
+    explorer = gui.ABFExplorer(startup_dir=cmd_args.startup_dir)
+    assert (
+        explorer.fileExplorerWidget.listbox_file_list.item(0).text() == "20101001.abf"
+    )
+    abfs_in_test_dir = sorted(
+        [
+            os.path.join(ABF_DATA_DIR, f)
+            for f in os.listdir(ABF_DATA_DIR)
+            if f.endswith(".abf")
+        ]
+    )
+    assert explorer.fileExplorerWidget.listbox_file_list.count() == len(
+        abfs_in_test_dir
     )
