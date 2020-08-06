409754d2f158fe85441332fb88fa2d519bef1b12
Nicholas George <nicholas.george32@gmail.com>
2020-08-06 16:19:23 -0600

    black

--- abf_explorer/__main__.py
+++ abf_explorer/__main__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(
-   0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 )  # hack for absolute imports/tests for now..
 import PyQt5.QtWidgets as qt
 from args import parser
