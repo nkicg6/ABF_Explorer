785cfda3a792d1c629b363a5111dc1bffda8830c
Nicholas George <nicholas.george32@gmail.com>
2020-08-08 11:59:14 -0600

    clean up main. fix imports

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="abf_explorer",
     version="0.1-dev",
-    description="A GUI to quickly visualize and explore axon binary format electrophysiology files",
+    description="A GUI to quickly visualize ABF electrophysiology files",
     packages=["abf_explorer"],
     install_requires=["numpy", "PyQt5", "pyqtgraph", "pyabf"],
     entry_points={"gui_scripts": ["abf_explorer = abf_explorer.__main__:main"]},
