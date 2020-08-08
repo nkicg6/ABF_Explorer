1ba98cb9ae85ba247a21f86966bccbdbc208e0d0
Nicholas George <nicholas.george32@gmail.com>
2020-08-08 12:21:13 -0600

    main edits

--- abf_explorer/__main__.py
+++ abf_explorer/__main__.py
@@ -8,13 +8,15 @@ from .abf_logging import make_logger
 
 logger = make_logger(__name__)
 
+# this is a this is a this is a this is a this is a this is a this is a this is a this is a
+
 
 def main():
+    """main for gui entrypoint"""
     cmd_args = parser.parse_args()
     app = qt.QApplication([])
     explorer = ABFExplorer(startup_dir=cmd_args.startup_dir)
     sys.exit(app.exec_())
-
 
 if __name__ == "__main__":
     logger.debug("Starting ABF_Explorer")
