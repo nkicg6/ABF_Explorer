e8702f75a469d52faeede03dab5c9c7bbb836ade
Nicholas George <nicholas.george32@gmail.com>
2020-11-08 22:55:30 -0700

    minor setup updates

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     long_description=README,
     long_description_content="text/markdown",
     packages=["abf_explorer"],
-    project_urls={"Source": "https://github.com/nkicg6/ABF_Explorer",},
+    url="https://github.com/nkicg6/ABF_Explorer",
+    classifiers=["License :: OSI Approved :: GNU GPL v3",
+        "Programming Language :: Python :: 3.7"],
     install_requires=["numpy", "PyQt5", "pyqtgraph", "pyabf"],
     entry_points={"gui_scripts": ["abf_explorer = abf_explorer.__main__:main"]},
 )
