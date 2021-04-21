4a82236b2dc0b8c8d489ce8f2ad979993036ce31
Nicholas George <nicholas.george32@gmail.com>
2021-04-21 10:22:26 -0600

    bump version to fix big sur gui failure. test passing

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     ],
     install_requires=[
         "numpy == 1.19.4",
-        "PyQt5 == 5.15.1",
+        "PyQt5 == 5.15.4",
         "pyqtgraph == 0.11.0",
         "pyabf == 2.2.8",
     ],
